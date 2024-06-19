4c866b09e4ff563c73b97f2c59ddf17195aa4c3c
Ngô Quang Bá <quangba2910@gmail.com>
2024-06-19 16:06:23 +0700

    update

--- server/src/services/report.service.js
+++ server/src/services/report.service.js
@@ -1,14 +1,14 @@
 import { response } from "../utils/lib.js"
-import { getOneDocument,  handleListQuery } from "../utils/queryFunction.js"
+import { getOneDocument } from "../utils/queryFunction.js"
 import Report from "../models/report.js"
 
 const fncCreateReport = async (req) => {
-    try {
-        const newCreateReport = await Report.create(req.body)
-        return response(newCreateReport, false, "Tạo Report thành công", 201)
-      } catch (error) {
-        return response({}, true, error.toString(), 500)
-      }
+  try {
+    const newCreateReport = await Report.create(req.body)
+    return response(newCreateReport, false, "Tạo Report thành công", 201)
+  } catch (error) {
+    return response({}, true, error.toString(), 500)
+  }
 }
 
 const fncGetReportDetail = async (req) => {
@@ -24,28 +24,28 @@ const fncGetReportDetail = async (req) => {
 
 const fncGetListReport = async (req) => {
   try {
-  const {CurrentPage, PageSize } = req.body
-  const query = { IsDeleted: false };
-  const reports = Report
-  .find(query)
-  .skip((CurrentPage - 1) * PageSize)
-  .limit(PageSize);
-const total = Report.countDocuments(query); 
-const result = await handleListQuery([reports, total]);
-return response(
-  { List: result[0], Total: result[1] },
-  false,
-  "Lấy ra Report thành công",
-  200
-);
+    const { CurrentPage, PageSize } = req.body
+    const query = { IsDeleted: false };
+    const reports = Report
+      .find(query)
+      .skip((CurrentPage - 1) * PageSize)
+      .limit(PageSize);
+    const total = Report.countDocuments(query);
+    const result = await Promise.all([reports, total]);
+    return response(
+      { List: result[0], Total: result[1] },
+      false,
+      "Lấy ra Report thành công",
+      200
+    );
   } catch (error) {
-      return response({}, true, error.toString(), 500)
+    return response({}, true, error.toString(), 500)
   }
 }
 
 const fncDeleteReport = async (req) => {
   try {
-    const ReportID  = req.param.ReportID
+    const ReportID = req.param.ReportID
     const deletedReport = await Blog.findByIdAndUpdate(
       ReportID,
       { IsDeleted: true },
@@ -60,25 +60,25 @@ const fncDeleteReport = async (req) => {
 
 const fncChangeHandelReport = async (req) => {
   try {
-      const ReportID = req.param.ReportID
-      const changeHandle = await Blog.findByIdAndUpdate(
-        ReportID,
-        { IsHandle: true },
-        { new: true }
-      )
-      if (!changeHandle) return response({}, true, "Report không tồn tại", 200)
-      return response(changeHandle, false, "Cập nhật xử lý thành công", 200)
-    } catch (error) {
-      return response({}, true, error.toString(), 500)
-    }
+    const ReportID = req.param.ReportID
+    const changeHandle = await Blog.findByIdAndUpdate(
+      ReportID,
+      { IsHandle: true },
+      { new: true }
+    )
+    if (!changeHandle) return response({}, true, "Report không tồn tại", 200)
+    return response(changeHandle, false, "Cập nhật xử lý thành công", 200)
+  } catch (error) {
+    return response({}, true, error.toString(), 500)
+  }
 }
 
 const ReportService = {
-fncCreateReport,
-fncGetListReport,
-fncGetReportDetail,
-fncDeleteReport,
-fncChangeHandelReport
+  fncCreateReport,
+  fncGetListReport,
+  fncGetReportDetail,
+  fncDeleteReport,
+  fncChangeHandelReport
 }
-    
+
 export default ReportService
