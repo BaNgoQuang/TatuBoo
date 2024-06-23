91864dc1b4a5623949b1ecaa9e016da5d52ac19e
ngtaiduc110 <129880826+ngtaiduc110@users.noreply.github.com>
2024-06-23 11:13:40 +0700

    API DOC FOR bankinginfor

--- server/src/routes/bankinginfor.route.js
+++ server/src/routes/bankinginfor.route.js
@@ -10,7 +10,7 @@ const BankingInforRoute = express.Router()
  * @swagger
  * components:
  *  schemas:
- *    BankingInfors:
+ *    BankingInfor:
  *      type: object
  *      required: 
  *        - UserID
@@ -36,21 +36,106 @@ const BankingInforRoute = express.Router()
  *            type: Number
  */
 
+/**
+ * @swagger
+ * /bankinginfor/createBankingInfor:
+ *   post:
+ *     tags: [BankingInfor]
+ *     requestBody:
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *                UserID:
+ *                  type: ObjectId
+ *                BankID: 
+ *                  type: number
+ *                UserBankName:
+ *                  type: string
+ *                UserBankAccount:
+ *                  type: number
+ *     responses:
+ *       201:
+ *         description: Created
+ *       400:
+ *         description: Bad Request
+ *       500:
+ *         description: Internal server error
+ */
 BankingInforRoute.post("/createBankingInfor",
   authMiddleware([Roles.ROLE_STUDENT, Roles.ROLE_TEACHER]),
   BankingInforController.createBankingInfor
 )
 
+/**
+ * @swagger
+ * /bankinginfor/getDetailBankingInfor:
+ *   get:
+ *     tags: [BankingInfor]
+ *     responses:
+ *       200:
+ *         description: Thông tin banking tồn tại
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Server error
+ */
 BankingInforRoute.get("/getDetailBankingInfor",
   authMiddleware([Roles.ROLE_STUDENT, Roles.ROLE_TEACHER]),
   BankingInforController.getDetailBankingInfor
 )
 
+/**
+ * @swagger
+ * /bankinginfor/deleteBankingInfor/{BankingInforID}:
+ *   get:
+ *     tags: [BankingInfor]
+ *     parameters:
+ *       - in: path
+ *         name: BankingInforID
+ *         schema:
+ *           type: ObjectId
+ *     responses:
+ *       200:
+ *         description: Xóa thông tin banking thành công
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Server error
+ */
 BankingInforRoute.get("/deleteBankingInfor/:BankingInforID",
   authMiddleware([Roles.ROLE_STUDENT, Roles.ROLE_TEACHER]),
   BankingInforController.deleteBankingInfor
 )
 
+/**
+ * @swagger
+ * /bankinginfor/updateBankingInfor:
+ *   post:
+ *     tags: [BankingInfor]
+ *     requestBody:
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *                BankingInforID:
+ *                  type: ObjectId
+ *                BankID:
+ *                  type: number
+ *                UserBankName: 
+ *                  type: string
+ *                UserBankAccount:
+ *                  type: number
+ *     responses:
+ *       200:
+ *         description: Cập nhật thông tin Banking thành công
+ *       400:
+ *         description: Bad Request
+ *       500:
+ *         description: Internal server error
+ */
 BankingInforRoute.post("/updateBankingInfor",
   authMiddleware([Roles.ROLE_STUDENT, Roles.ROLE_TEACHER]),
   BankingInforController.updateBankingInfor
