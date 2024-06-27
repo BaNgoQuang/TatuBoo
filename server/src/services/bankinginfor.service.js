3de178e21e5134cdade204d9f503880cdfe82942
ngtaiduc110 <129880826+ngtaiduc110@users.noreply.github.com>
2024-06-27 23:02:11 +0700

    update GetListPaymentInCurrentWeek

--- server/src/services/bankinginfor.service.js
+++ server/src/services/bankinginfor.service.js
@@ -125,24 +125,24 @@ const fncGetListPaymentInCurrentWeek = async (req) => {
     const teacherData = [];
     for (const teacherId in teacherCounts) {
       const teacherBankingInfor = await BankingInfor.findOne({ User: teacherId })
-
       const teacherName = await User.findById(teacherId).then((user) => user.FullName);
       const teacherPrice = await User.findById(teacherId).then((user) => user.Price);
       const salary = (teacherPrice * teacherCounts[teacherId])
-      const teacherPayment = await Payment.findOne({ Receiver: teacherId, PaymentTime: { $gte: startOfWeek, $lte: endOfWeek } })
+      let teacherPayment = await Payment.findOne({ Receiver: teacherId, PaymentTime: { $gte: startOfWeek, $lte: endOfWeek } })
 
-      if (!teacherPayment) {
-        let createPayment = await Payment.create({
+      if (teacherPayment == null) {
+        const createPayment = await Payment.create({
           Sender: "664a5251b0563919ce2eba19",
           Receiver: teacherId,
           FeeType: 3,
           TraddingCode: Math.floor(Math.random() * 10000),
           TotalFee: salary,
-          Description: "Thanh toán tiền dạy học cho giảng viên" + teacherName,
+          Description: "Thanh toán tiền dạy học cho giảng viên " + teacherName,
           PaymentStatus: 0,
         })
+        teacherPayment = createPayment
       }
-      if (teacherPayment == null) teacherPayment = createPayment
+
       teacherData.push({
         teacherId: teacherId,
         teacherName,
